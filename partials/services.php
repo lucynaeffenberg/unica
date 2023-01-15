f79163ff853c9a32927192bfbdd12a602bfce751
lucynaeffenberg <lucyna.effenberg@gmail.com>
2023-01-15 21:18:09 +0100

    Update

--- partials/services.php
+++ partials/services.php
@@ -2,5 +2,31 @@
 	<div class="container">
 		<h2 class="text-center">Our Services</h2>
 		<p class="text-center">We provides the opportunity to prepare for life</p>
+		<div class="flex-cols flex-cols-3 row-gap-30">
+			<div>
+				<div>
+					<div>img</div>
+					<div>
+						h3
+						p
+					</div>
+				</div>
+			</div>
+			<div>
+				Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.
+			</div>
+			<div>
+				Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.
+			</div>
+			<div>
+				Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.
+			</div>
+			<div>
+				Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.
+			</div>
+			<div>
+				Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.
+			</div>
+		</div>
 	</div>
 </section>
