3b919c5982b6ef12686e476e3335ba4a5dd11239
lucynaeffenberg <lucyna.effenberg@gmail.com>
2023-01-15 17:02:20 +0100

    Update

---
+++ content.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Title</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<header>
+			<?php include('partials/header.html') ?>
+		</header>
+		<main>
+			Content
+			<?php include('partials/newsletter.html') ?>
+		</main>
+		<footer>
+			<?php include('partials/footer.html') ?>
+		</footer>
+	</body>
+</html>
