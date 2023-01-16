9bfdc564cd2ae9b8f211ef262f0a4e4fc168466e
lucynaeffenberg <lucyna.effenberg@gmail.com>
2023-01-16 12:12:01 +0100

    Update

--- partials/services.php
+++ partials/services.php
@@ -4,11 +4,13 @@
 		<p class="text-center">We provides the opportunity to prepare for life</p>
 		<div class="flex-cols flex-cols-3 row-gap-30">
 			<div>
-				<div>
-					<div>img</div>
+				<div class="d-flex column-gap-30">
 					<div>
-						h3
-						p
+						<img class="h-45" src="assets/img/icons/icon-walkman.svg" alt="">
+					</div>
+					<div>
+						<h3 class="mt-0">Art Studio</h3>
+						<p>Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue. Aenean dos cursus lania.</p>
 					</div>
 				</div>
 			</div>
